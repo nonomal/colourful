1164eb9e31469fabc2f8376aae53f5c73b94b5e5
liuxudong <1171866049@qq.com>
2022-11-12 16:35:10 +0800

    add look cookie from env

--- config/config.go
+++ config/config.go
@@ -35,6 +35,16 @@ func Load() *Config {
 		panic(err)
 	}
 	yaml.Unmarshal(content, config)
+	if config.Baidu.Cookie == "" {
+		log.Println("Baidu Cookie is empty in config file")
+		cookie, ok := os.LookupEnv("BAIDU_COOKIE")
+		if ok {
+			config.Baidu.Cookie = cookie
+			log.Println("Baidu Cookie is loaded from env")
+		} else {
+			log.Println("Baidu Cookie is empty in env")
+		}
+	}
 	log.Printf("Loaded config: %+v", config)
 	return config
 }
